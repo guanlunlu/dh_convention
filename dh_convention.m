49cf6fac9e60f2a38962c9344e974fad80697586
guanlunlu <guanlunlu@gapp.nthu.edu.tw>
2022-10-17 02:02:34 +0800

    bug fixed

--- dh_convention.m
+++ dh_convention.m
@@ -5,4 +5,4 @@ T_2to1 = dh_transform_i_to_i_1(t_2, 0, 325, 0)
 T_3to2 = dh_transform_i_to_i_1(t_3, 0, 275, 0)
 T_4to3 = dh_transform_i_to_i_1(0, 0, 0, d_4)
 
-T_4to0 = T_1to0*T_2t01*T_3to2*T_4to3
+T_4to0 = T_1to0*T_2to1*T_3to2*T_4to3
